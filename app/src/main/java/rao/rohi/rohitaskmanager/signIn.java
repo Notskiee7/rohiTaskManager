e3e8256724e2de589a139b95cf4bc84a98f2d1d2
karawan <banat811@gmail.com>
2022-09-08 14:58:46 +0300

    splash Screen

---
+++ app/src/main/java/rao/rohi/rohitaskmanager/signIn.java
@@ -0,0 +1,14 @@
+package rao.rohi.rohitaskmanager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class signIn extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+    }
+}
