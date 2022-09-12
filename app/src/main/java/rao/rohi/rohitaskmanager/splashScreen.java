d82c563df3730191eed6ec6d1776a9c7d6debba2
karawan <banat811@gmail.com>
2022-09-12 14:13:14 +0300

    splash screen code finish ,, next time we should test it

--- app/src/main/java/rao/rohi/rohitaskmanager/splashScreen.java
+++ app/src/main/java/rao/rohi/rohitaskmanager/splashScreen.java
@@ -2,7 +2,9 @@ package rao.rohi.rohitaskmanager;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class splashScreen extends AppCompatActivity {
 
@@ -10,5 +12,17 @@ public class splashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+        //start next activity screen automatically after period of time
+        Handler h = new Handler();
+        Runnable r = new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(splashScreen.this,signIn.class);
+                startActivity(i);                   //to start the i wich is going from splash activity to signIn
+                finish();                          //to close the current activity
+            }
+        };
+        h.postDelayed(r,3000);            //delay the r 3 seconds
     }
+
 }
