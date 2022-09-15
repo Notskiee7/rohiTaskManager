69e50ffc0834fa57663cfdc1a1611714c672f08a
karawan <banat811@gmail.com>
2022-09-15 11:28:46 +0300

    we did all the private and findview , and i linked the sign up button with sign up activity

--- app/src/main/java/rao/rohi/rohitaskmanager/SignUp.java
+++ app/src/main/java/rao/rohi/rohitaskmanager/SignUp.java
@@ -3,12 +3,33 @@ package rao.rohi.rohitaskmanager;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+
+import com.google.android.material.textfield.TextInputEditText;
 
 public class SignUp extends AppCompatActivity {
+    private TextInputEditText etMail2;
+    private TextInputEditText etPassword2;
+    private TextInputEditText etConfirmpassword;
+    private Button btnSave;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_up2);
+        setContentView(R.layout.activity_sign_up);
+
+        etMail2=findViewById(R.id.etMail2);
+        etPassword2=findViewById(R.id.etPassword2);
+        etConfirmpassword=findViewById(R.id.etConfirmpassword);
+        btnSave=findViewById(R.id.btnSave);
+
+
+
+
+
+
+
     }
 }
--- app/src/main/java/rao/rohi/rohitaskmanager/signIn.java
+++ app/src/main/java/rao/rohi/rohitaskmanager/signIn.java
@@ -2,14 +2,42 @@ package rao.rohi.rohitaskmanager;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.material.textfield.TextInputEditText;
 
 public class signIn extends AppCompatActivity {
+
+    private TextInputEditText etMail;
+    private TextInputEditText etPassword;
+    private Button btnSignup;
+    private Button btnSignin;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+
+        etMail=findViewById(R.id.etMail);
+        etPassword= findViewById(R.id.etPassword);
+        btnSignup=findViewById(R.id.btnSignup);
+        btnSignin=findViewById(R.id.btnSignin);
+
+
+
+        btnSignup.setOnClickListener(new View.OnClickListener()      //this function linked the button sign up with the activity sign up
+        {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(signIn.this,SignUp.class);
+                startActivity(i);
+            }
+        });
 
 
 
