9de1cc263d2211a6feef600b2022c5bc7880fb3b
karawan <banat811@gmail.com>
2022-09-27 13:26:09 +0300

    setting and history and sign out item with conditions

--- app/src/main/java/rao/rohi/rohitaskmanager/MainActivity.java
+++ app/src/main/java/rao/rohi/rohitaskmanager/MainActivity.java
@@ -1,10 +1,13 @@
 package rao.rohi.rohitaskmanager;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.SearchableInfo;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ListView;
@@ -33,5 +36,40 @@ public class MainActivity extends AppCompatActivity {
 
 
 
+
+
     }
+
+    @Override
+    //a function that returns true if all things are alright (no bugs)
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu,menu);      //to make menu appear in the emulator
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId()==R.id.ItmSettings){
+            Intent i = new Intent(MainActivity.this,Settings.class);
+            startActivity(i);
+        }
+
+        else if(item.getItemId()==R.id.ItmSignOut){
+            Intent i = new Intent(MainActivity.this,signIn.class);
+            startActivity(i);
+        }
+        else if (item.getItemId()==R.id.ItmHistory){
+            Intent i = new Intent(MainActivity.this,History.class);
+            startActivity(i);
+
+        }
+        return true;
+    }
+
+
+
+
+
+
+
 }
---
+++ app/src/main/java/rao/rohi/rohitaskmanager/Settings.java
@@ -0,0 +1,15 @@
+package rao.rohi.rohitaskmanager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Settings extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+
+    }
+}
